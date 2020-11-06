52fe7a778432fe1d00dc837af723261fbc9fc0c0
rafacoelho4 <rafaelmalto@hotmail.com>
2020-11-06 12:43:40 -0300

    User routes

--- src/database/migrations/1602649440643-create_orphanages.ts
+++ src/database/migrations/1602649440643-create_orphanages.ts
@@ -42,8 +42,22 @@ export class createOrphanages1602649440643 implements MigrationInterface {
           name: 'open_on_weekends',
           type: 'boolean',
           default: false
+        },
+        {
+          name: 'user_id',
+          type: 'string',
+        },
+      ],
+      foreignKeys: [
+        {
+            name: 'OrphanageUser',
+            columnNames: ['user_id'],
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            onUpdate: 'CASCADE',
+            onDelete: 'CASCADE',
         }
-      ]
+      ],
     }));
   }
 
--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,8 @@ import multer from 'multer';
 
 import uploadConfig from './config/upload';
 import OrphanagesController from './controllers/OrphanagesController';
+import UserController from './controllers/UserController';
+import authController from './controllers/authController';
 
 const routes = Router();
 const upload = multer(uploadConfig);
@@ -10,5 +12,12 @@ const upload = multer(uploadConfig);
 routes.get('/orphanages', OrphanagesController.index);
 routes.get('/orphanages/:id', OrphanagesController.show);
 routes.post('/orphanages', upload.array('images'), OrphanagesController.create);
+
+routes.get('/users', UserController.index);
+routes.get('/users/:id', UserController.show);
+routes.post('/users', UserController.create);
+routes.delete('/users/:id', UserController.destroy);
+
+routes.post('/auth', authController.authenticate);
 
 export default routes;
