3ca1b769b518170ad68c8cbe0b457531bdbce7cb
rafacoelho4 <rafaelmalto@hotmail.com>
2020-11-12 17:47:43 -0300

    Update method

--- ormconfig.js
+++ ormconfig.js
@@ -5,7 +5,6 @@ module.exports = {
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
   database: process.env.TYPEORM_DATABASE,
-  synchronize: true,
   entities: [process.env.TYPEORM_ENTITIES],
   migrations: [process.env.TYPEORM_MIGRATIONS],
   cli: {
